792623c53fe95c66e8ee5dd0e8e92f0c974f70b5
Jimmy Zhan <nuttertools5790@gmail.com>
2023-06-15 11:36:10 +0800

    整理edit.js 加上sweetalert

--- src/main/webapp/WEB-INF/member/js/edit.js
+++ src/main/webapp/WEB-INF/member/js/edit.js
@@ -17,19 +17,24 @@
     const inputs = document.querySelectorAll('input');
     const confirmbtn = document.querySelector('#confirmbtn');
     const avatarpreview = document.querySelector('#avatar-preview')
-    const avatarUpload = document.getElementById("inputmemberPic");
     const avatarPlaceholder = document.getElementById("avatar-placeholder");
     const avatarPreview = document.getElementById("avatar-preview");
-
 
     const canslebtn = document.querySelector('#canslebtn');
     const confirmbtn2 = document.querySelector('#confirmbtn2');
     const cancelbtn2 = document.querySelector('#cancelbtn2');
     const msg = document.querySelector('#msg');
     const memberId = sessionStorage.getItem('memberNo');
+
+    const oldmemberPassword = document.querySelector('#oldmemberPassword')
+    const newmemberPassword = document.querySelector('#newmemberPassword')
+    const newmemberPasswordConfirm = document.querySelector('#newmemberPasswordConfirm')
     console.log("edit.js啟動");
 
-    // 查資料回來========================================================
+
+    // ===============================VVV方法區VVV====================================
+
+    // ============================1.查資料回來getmemberdata()========================
     function getmemberdata() {//取瀏覽器的memberid 去查出來
         console.log('進入getmemberdata')
         fetch("getmemberdata", {
@@ -62,7 +67,6 @@
                     memberStat
                 } = body;
                 console.log(body);
-                console.log('取完值 要放上input')
                 inputmemberNo.value = memberNo;
                 inputmemberAccount.value = memberAccount;
                 inputmemberName.value = memberName;
@@ -76,7 +80,7 @@
                 inputmemberPhone.value = memberPhone;
                 inputmemberEmail.value = memberEmail;
 
-                if(typeof memberAddress !== 'undefined'){
+                if (typeof memberAddress !== 'undefined') {
                     inputmemberAddress.value = memberAddress;
                 }
 
@@ -99,9 +103,9 @@
                     avatarPreview.style.display = "none";
                     avatarPlaceholder.style.display = "block"; // 顯示灰色區塊
                 }
-                if(typeof memberCard !== 'undefined'){
+                if (typeof memberCard !== 'undefined') {
                     inputmemberCard.value = memberCard;
-                }else {
+                } else {
                     inputmemberCard.value = 0;
                 }
 
@@ -124,38 +128,10 @@
             });
 
     };
-    //============總之先查一次===============================
-    getmemberdata();
 
-// 性別input控制=====================================================================
-    const selectGender = document.querySelector('#selectGender');
-    selectGender.addEventListener('change', (e) => {
-        const selectedValue = selectGender.value;
-        inputmemberGender.value = selectedValue;
-    })
-    //重製按鈕===========================================================================
-    canslebtn.addEventListener('click', () => {
-        getmemberdata();
-    });
+    // ============================2. 修改資料進去 editmemberdata()========================
+    function editmemberdata() {
 
-    //讀取圖片 轉成base64==================================================
-    let base64Image = '';
-    inputmemberPic.addEventListener("change", function (event) {
-        const file = event.target.files[0]; // 獲取選擇的檔案
-        if (file) {
-            const reader = new FileReader(); //讀取
-            reader.onload = function (e) {
-                const imageSrc = e.target.result; // 獲取數據
-                base64Image = imageSrc.split(",")[1];// 轉成base64
-
-            };
-            reader.readAsDataURL(file); // 讀取成url
-        }
-    });
-
-
-// 確認修改按鈕=================================================================
-    confirmbtn.addEventListener('click', () => {
         // 前端確認資料填寫
         msg.textContent = ' ';
         console.log("確認修改按鈕啟動");
@@ -186,13 +162,14 @@
         // }
 
         const memberCarda = inputmemberCard.value;
+        const memberCardLength = inputmemberCard.value.length;
         if (memberCarda != 0) {
             if (memberCardLength < 15 || memberCardLength > 19) {
                 msg.textContent = '信用卡長度須介於15~19碼';
                 return;
             }
         }
-       const Birthday= inputmemberBirthday.value;
+        const Birthday = inputmemberBirthday.value;
         if (!Birthday) {
             msg.textContent = '生日為必填';
             return;
@@ -237,18 +214,104 @@
                 const {successful} = body;//const successful = body.successful;
                 const {memberAccount} = body;
                 if (successful) {
-                    // for (let input of inputs) {
-                    //     input.disabled = true;
-                    // }
                     msg.className = 'info';
                     msg.textContent = '修改成功';
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'success',
+                        title: '修改成功!',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
                 } else {
                     msg.className = 'error';
                     msg.textContent = '修改失敗';
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: '修改失敗!',
+                        footer: '<a href=""></a>'
+                    })
                 }
             });
 
-    });
+    }
+    // ============================3. 圖片讀取，轉型成base64 readPic()========================
+    let base64Image = '';
+    function readPic(event){
+        const file = event.target.files[0]; // 獲取選擇的檔案
+        if (file) {
+            const reader = new FileReader(); //讀取
+            reader.onload = function (e) {
+                const imageSrc = e.target.result; // 獲取數據
+                base64Image = imageSrc.split(",")[1];// 轉成base64
 
+            };
+            reader.readAsDataURL(file); // 讀取成url
+        }
+    }
+    // ============================3. 圖片讀取，轉型成base64 readPic()========================
+    function resetform(){
+        inputmemberNo.value = "";
+        inputmemberAccount.value = "";
+        inputmemberName.value = "";
+        inputmemberGender.value = "";
+        inputmemberPhone.value = "";
+        inputmemberEmail.value = "";
+        inputmemberJoinTime.value = "";
+        inputlevelNo.value = "";
+        inputmemberBirthday.value = "";
+        inputmemberNation.value = "";
+        inputmemberPoints.value = "";
+        inputmemberStat.value = "";
+        inputmemberAccount.value = "";
+        inputmemberPic.type = "text";
+        inputmemberPic.type = "file";//清空圖片
+        inputmemberAddress.value = "";
+        inputmemberCard.value = "";
+        msg.textContent = '';
+    }
+    // ===============================^^^方法區^^^====================================
+
+    // ===============================VVV使用方法區VVV================================
+    //=================================1. 總之先查一次=================================
+    getmemberdata();
+    // ===============================2. 確認修改按鈕================================
+    confirmbtn.addEventListener('click', () => {
+        editmemberdata();
+    });
+    //=================================3. 圖片檔案上傳按鈕=============================
+    inputmemberPic.addEventListener("change", function (event) {
+        readPic(event);
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: '上傳成功!',
+            showConfirmButton: false,
+            timer: 1500
+        })
+    });
+    //=================================3. 重製按鈕(再查一次)=============================
+    canslebtn.addEventListener('click', () => {
+        resetform();
+        setTimeout(function() {
+            console.log("等1秒");
+            getmemberdata();
+        }, 1000);
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: '已刷新!',
+            showConfirmButton: false,
+            timer: 1500
+        })
+    });
+    // ================================4. 性別select 轉貼至input====================================
+    const selectGender = document.querySelector('#selectGender');
+    selectGender.addEventListener('change', (e) => {
+        const selectedValue = selectGender.value;
+        inputmemberGender.value = selectedValue;
+    })
+    // ===============================^^^使用方法區^^^==============================
 
 })();
